ac66d5a64b0a1063ddfa3237d852550c00ff5f94
adrncrlvrgs <imadrianvargas@gmail.com>
2025-05-12 16:19:02 +0800

    add cors and cookieparser

--- app.ts
+++ app.ts
@@ -1,7 +1,19 @@
 import express from "express";
+import cors from 'cors'
+import cookieParser from "cookie-parser";
 
 const port = 8000;
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+  })
+)
 
 app.get("/", (req, res) => {
   res.send("Hello camplyr!!!");
