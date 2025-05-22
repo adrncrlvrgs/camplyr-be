946312b757abcd79f1784e60839e5201d1d843bd
adrncrlvrgs <imadrianvargas@gmail.com>
2025-05-22 21:33:11 +0800

    todo

--- server.ts
+++ server.ts
@@ -5,3 +5,9 @@ const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is now running at ${PORT}`);
 });
+
+
+// to do
+// connect be and fe
+// create initaial working login
+// landing page
