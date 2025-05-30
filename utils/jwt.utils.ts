812ee35ea754c51689b686b8c2d8232f3307dca9
adrncrlvrgs <imadrianvargas@gmail.com>
2025-05-30 21:54:52 +0800

    add jwt util fnc

--- utils/jwt.utils.ts
+++ utils/jwt.utils.ts
@@ -2,3 +2,6 @@ import jwt from "jsonwebtoken";
 
 export const signIn = (payload: object) =>
   jwt.sign(payload, process.env.JWT_SECRET!, { expiresIn: "7d" });
+
+export const verify = (token: string) =>
+  jwt.verify(token, process.env.JWT_SECRET!);
