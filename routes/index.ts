ac54fa7f1b00cae1a533f7c16743d0e3e10d3aa0
Adrian Vargas <imadrianvargas@gmail.com>
2026-08-19 19:20:23 +0800

    add update job routes

--- routes/index.ts
+++ routes/index.ts
@@ -2,12 +2,13 @@ import express from "express";
 import authRoutes from "../routes/auth.routes";
 import onboardRoutes from "../routes/onboard.routes"
 import postRoutes from "../routes/post.routes"
+import jobRoutes from "../routes/job.routes"
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/onboard", onboardRoutes);
 router.use("/post", postRoutes)
-// router.use("/job")
+router.use("/job",jobRoutes )
 
 export default router;
